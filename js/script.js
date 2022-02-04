8a2eb7bc93a3fa4de82ecdd142e279021691fa6e
Piotr Kozak <piotrekkozak9@gmail.com>
2022-02-04 18:59:09 +0100

    Add results to the game

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
+//Computer move
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
 
 if(randomNumber == 1){
     computerMove = 'kamień';
@@ -15,10 +19,13 @@ else if(randomNumber == 3){
         printMessage('Ruch komputera to: ' + computerMove);
 }
 
+//Users move
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
+let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
     playerMove = 'kamień';
@@ -31,4 +38,34 @@ else if (playerInput == '2'){
 else if (playerInput == '3'){
     playerMove = 'papier';
         printMessage('Twój ruch to: ' + playerMove);
+}
+
+//Results
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Wygrywasz!');
+}
+else if (computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Niestety przegrywasz')
+}
+else if (computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis')
+}
+else if (computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Wygrywasz!')
+}
+else if (computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Niestety przegrywasz')
+}
+else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Remis')
+}
+else if (computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Wygrywasz!')
+}
+else if (computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Niestety przegrywasz')
+}
+else if (computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Remis')
 }
